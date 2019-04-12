8bac2fed0ef0d8f54eb1138e95310ad21c3604c3
Zhichao Jin <jinzcdev@icloud.com>
2019-04-12 10:33:08 +0800

    update .gitignore -- ignore /test directory

--- Advanced/A1135 Is It A Red-Black Tree.cpp
+++ Advanced/A1135 Is It A Red-Black Tree.cpp
@@ -9,7 +9,8 @@ int k,n;
 vector<int> pre,in;
 
 struct node{
-	int data,color;
+	int data;
+	bool isVisit;
 	node *lchild,*rchild;	
 };
 
@@ -17,8 +18,8 @@ node* create(int preL,int preR,int inL,int inR){
 	if(inL > inR) return NULL;
 	node* root = new node;
 	root -> data = pre[preL];
-	if(root -> data > 0) root -> color = 0; //black
-	else root -> color = -1;		//red
+	root -> isVisit = false;
+	root -> lchild = root -> rchild = NULL;
 	int k;
 	for(int i = inL;i <= inR;i++){
 		if(pre[preL] == in[i]){
@@ -36,14 +37,6 @@ bool cmp(int a,int b){
 	return abs(a) < abs(b);
 }
 
-void post(node* root){
-	if(root != NULL){
-		post(root->lchild);
-		post(root->rchild);
-		printf("%d  ", root ->data);
-	}
-}
-
 // determine if the root is red.if true,return false,or not,return true.
 bool judgeRoot(node* root){
 	if(root -> data < 0) return false;
@@ -52,8 +45,8 @@ bool judgeRoot(node* root){
 
 //determine if the children of the red node are black.
 bool judgeRedNode(node* p){
-	if(p -> lchild != NULL && p -> data < 0) return false;
-	if(p -> rchild != NULL && p -> data < 0) return false;
+	if(p -> lchild != NULL && p -> lchild -> data < 0) return false;
+	if(p -> rchild != NULL && p -> rchild -> data < 0) return false;
 	return true;
 }
 
@@ -75,6 +68,41 @@ bool judgeTreePreOrder(node* root){
 	return is;
 }
 
+bool judge(node* root){
+	if(root == NULL || root -> data < 0) return false;
+	bool is = true,isFirst = true;
+	stack<node*> s;
+	node* p = root;
+	int count = 0,bns;	//the sum of black node.
+	while(p != NULL || !s.empty()){
+		if(p != NULL){
+			// visit(p)
+			// printf("%d ",p -> data);
+			if(p -> data < 0 && !judgeRedNode(p)) {printf(" -- ");return false;}
+			if(p -> data > 0) count++;
+			s.push(p);
+			p = p -> lchild;
+		}else{	// have visited the leaf node.
+			// count++;
+			if(isFirst){
+				bns = count;
+				isFirst = false;
+			}else if(bns != count){printf("%d -- ",s.top() -> data); return false;}
+			p = s.top();
+			if(p -> isVisit){
+				if(p -> data > 0) count--;
+				s.pop();
+				p = s.top() -> rchild;
+				printf("%d ------\n", p -> data);
+			}else{
+				p -> isVisit = true;
+				p = p -> rchild;
+			}
+		}
+	}
+	return is;
+}
+
 int main(){
 	int t;
 	scanf("%d",&k);
@@ -87,7 +115,11 @@ int main(){
 		}
 		sort(in.begin(),in.end(),cmp);
 		node* root = create(0,n - 1,0,n - 1);
-	//	isRedBlackTree(root);
+		// if(!judgeRoot(root)) printf("NO\n");
+		// else if(!judgeTreePreOrder(root)) printf("NO\n");
+		// else printf("TRUE\n");
+		if(judge(root)) printf("YES\n");
+		else printf("NO\n");
 		free(root);
 		pre.clear();
 		in.clear();
---
+++ Basic/B1017 A除以B(20).cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstring>
+const int n = 1010;
+char a[n];
+int b;
+
+int getRemainder(int ){
+
+}
+
+int main(){
+	scanf("%s %d",a,&b);
+	int len = strlen(a),mid = len / 2,rmd;
+	getRemainder(0,mid - 1,mid,len - 1);
+	return 0;
+}
