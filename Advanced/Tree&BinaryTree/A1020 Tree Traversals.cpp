1a5e5273baf850323b16a63c87a080d9388bae34
Zhichao Jin <jinzcdev@icloud.com>
2019-08-14 15:00:54 +0800

    Add New.

---
+++ Advanced/Tree&BinaryTree/A1020 Tree Traversals.cpp
@@ -0,0 +1,52 @@
+// https://pintia.cn/problem-sets/994805342720868352/problems/994805485033603072
+#include <cstdio>
+#include <queue>
+using namespace std;
+const int maxn = 40;
+struct node {
+    int data;
+    node* lchild;
+    node* rchild;
+};
+
+int n, post[maxn], in[maxn];
+
+void levelorder(node* root){
+    queue<node*> q;
+    q.push(root);
+    int cnt = 0;
+    while (!q.empty()){
+        node* now = q.front();
+        q.pop();
+        printf("%d", now -> data);
+        if (++cnt < n) printf(" ");
+        if (now -> lchild != NULL) q.push(now -> lchild);
+        if (now -> rchild != NULL) q.push(now -> rchild);
+    }
+}
+
+node* create(int postL, int postR, int inL, int inR){
+    node* root = new node;
+    root -> data = post[postR];
+    int k = inL;
+    while (in[k] != post[postR]){
+        k++;
+    }
+    int numLeft = k - inL;
+    root -> lchild = create(postL, postL + numLeft - 1, inL, k - 1);
+    root -> rchild = create(postL + numLeft, postR - 1, k + 1, inR);
+    return root;
+}
+
+int main(){
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        scanf("%d", &post[i]);
+    }
+    for (int i = 0; i < n; i++){
+        scanf("%d", &in[i]);
+    }
+    node* root = create(0, n - 1, 0, n - 1);
+    levelorder(root);
+    return 0;
+}
