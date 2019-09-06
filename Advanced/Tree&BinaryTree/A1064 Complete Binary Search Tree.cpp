acf2d46f3e97070a5f73274ee18cac1fd5d57a83
Zhichao Jin <jinzcdev@icloud.com>
2019-09-06 20:40:12 +0800

    Update A1064 Complete Binary Search Tree

--- Advanced/Tree&BinaryTree/A1064 Complete Binary Search Tree.cpp
+++ Advanced/Tree&BinaryTree/A1064 Complete Binary Search Tree.cpp
@@ -3,13 +3,8 @@
 #include <algorithm>
 using namespace std;
 const int maxn = 1010;
-int n, in[maxn], CBT[maxn];
+int n, in[maxn], CBT[maxn], index = 0;
 
-bool cmp(int a, int b){
-    return a < b;
-}
-
-int index = 0;
 void inOrder(int root){         // 中序遍历空的完全二叉排序(根结点序号为1), 遍历同时作赋值, 中序序列为有序序列
     if (root > n) return;
     inOrder(root * 2);
@@ -22,7 +17,7 @@ int main(){
     for (int i = 0; i < n; i++){
         scanf("%d", &in[i]);
     }
-    sort(in, in + n, cmp);      // 升序结点数组获得中序序列
+    sort(in, in + n);      // 升序结点数组获得中序序列
     inOrder(1);
     for (int i = 1; i <= n; i++){
         printf("%d", CBT[i]);
