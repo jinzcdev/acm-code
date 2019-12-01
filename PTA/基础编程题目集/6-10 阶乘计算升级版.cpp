bf7e2091e7faf987903d64a3dfe077843bdc3771
Zhichao Jin <jinzcdev@icloud.com>
2019-12-01 20:52:13 +0800

    6-7 统计某类完全平方数 | 6-10 阶乘计算升级版

---
+++ PTA/基础编程题目集/6-10 阶乘计算升级版.cpp
@@ -0,0 +1,23 @@
+// https://pintia.cn/problem-sets/14/problems/742
+#include <stdio.h>
+#include <math.h>
+void Print_Factorial ( const int N ) {
+    int ans[1000];
+    ans[0] = 1;
+    for (int i = 1; i <= N; i++) {
+
+    }
+    
+
+}
+
+int main()
+{
+    // int N;
+	
+    // scanf("%d", &N);
+    // Print_Factorial(N);
+    printf("%d", (int)sqrt(145));
+    
+    return 0;
+}
