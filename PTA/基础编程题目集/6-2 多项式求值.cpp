5c24e3be092b636c14906695e37d23075cd976c7
Zhichao Jin <jinzcdev@icloud.com>
2019-12-01 20:51:22 +0800

    6-2 多项式求值

--- PTA/基础编程题目集/6-2 多项式求值.cpp
+++ PTA/基础编程题目集/6-2 多项式求值.cpp
@@ -3,25 +3,36 @@
 
 #define MAXN 10
 
-double f( int n, double a[], double x, int Max );
+// double f( int n, double a[], double x, int Max );
+double ff( int n, double a[], double x, int MAX) {
+    // a[0] + x * (a[1] + x * a[2]);
+    if (n == 0) return a[n];
+    return a[MAX - n] + x * ff(n - 1, a, x, MAX);
+}
 
+
+
+double f( int n, double a[], double x ) {
+    return ff(n, a, x, n);
+}
 int main()
 {
-    int n, i;
-    double a[MAXN], x;
+    int n = 2, i;
+    double a[MAXN] = {1, 2.5, -38.7}, x = 1.1;
 	
-    scanf("%d %lf", &n, &x);
-    for ( i=0; i<=n; i++ )
-        scanf("%lf", &a[i]);
-    printf("%.1f\n", f(n, a, x, n + 1));
+    // scanf("%d %lf", &n, &x);
+    // for ( i=0; i<=n; i++ )
+    //     scanf("%lf", &a[i]);
+    printf("%.1f\n", f(n, a, x));
     return 0;
 }
 
-double f( int n, double a[], double x, int Max) {
-    double k = a[Max - n - 1];
-    double l = f (n - 1, a, x, Max);
-    return k + x * l;
-}
+
+// double f( int n, double a[], double x, int Max) {
+//     double k = a[Max - n - 1];
+//     double l = f (n - 1, a, x, Max);
+//     return k + x * l;
+// }
 
 /*
 double f( int n, double a[], double x ) {
