989d5186722ea0509bccaeb19dca42ef1ca581c0
Zhichao Jin <jinzcdev@icloud.com>
2019-12-16 11:48:45 +0800

    B1071 小赌怡情

--- PAT/Basic/B1071 小赌怡情.cpp
+++ PAT/Basic/B1071 小赌怡情.cpp
@@ -1,38 +1,24 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int t, k, b, cost, n1, n2;
-    scanf("%d%d", &t, &k);
-    if (t <= 0) {
-        printf("Game Over.\n");
-        return 0;
-    }
+    int T, k, b, t, n1, n2;
+    scanf("%d%d", &T, &k);
     while (k--) {
-        scanf("%d%d%d%d", &n1, &b, &cost, &n2);
-        if (t < cost) {
-            printf("Not enough tokens.  Total = %d.\n", t);
+        scanf("%d%d%d%d", &n1, &b, &t, &n2);
+        if (T < t) {
+            printf("Not enough tokens.  Total = %d.\n", T);
             continue;
         }
-        if (n1 > n2) {
-            if (b == 0) {
-                t += cost;
-                printf("Win %d!  Total = %d.\n", cost, t);
-            } else {
-                t -= cost;
-                printf("Lose %d.  Total = %d.\n", cost, t);
+        if ((n1 > n2 && b == 0) || (n1 < n2 && b == 1)) {
+            T += t;
+            printf("Win %d!  Total = %d.\n", t, T);
+        } else {
+            T -= t;
+            printf("Lose %d.  Total = %d.\n", t, T);
+            if (T <= 0) {
+                printf("Game Over.\n");
+                break;
             }
-        } else if (n1 < n2) {
-            if (b == 1) {
-                t += cost;
-                printf("Win %d!  Total = %d.\n", cost, t);
-            } else {
-                t -= cost;
-                printf("Lose %d!  Total = %d.\n", cost, t);
-            }
-        }
-        if (t <= 0) {
-            printf("Game Over.\n");
-            break;
         }
     }
     return 0;
