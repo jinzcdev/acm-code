574085619d3c5e1ee3e1338413bc00b386aac66d
Zhichao Jin <jinzcdev@icloud.com>
2019-12-28 12:45:31 +0800

    PTA/基础编程题目集/函数题(全)

--- PTA/基础编程题目集/函数题/6-1 简单输出整数.c
+++ PTA/基础编程题目集/函数题/6-1 简单输出整数.c
@@ -1,21 +1,20 @@
 // https://pintia.cn/problem-sets/14/problems/733
 #include <stdio.h>
 
-void PrintN ( int N );
+void PrintN(int N);
 
-int main ()
-{
+int main() {
     int N;
 
     scanf("%d", &N);
-    PrintN( N );
+    PrintN(N);
 
     return 0;
 }
 
-void PrintN ( int N ){
+void PrintN(int N) {
     int i = 1;
-    while( i <= N ){
-        printf("%d\n",i++);
+    while (i <= N) {
+        printf("%d\n", i++);
     }
 }
