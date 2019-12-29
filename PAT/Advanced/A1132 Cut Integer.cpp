8408c0e223ee7856e139bd1a7a64697460f00e13
Zhichao Jin <jinzcdev@icloud.com>
2019-12-29 12:53:42 +0800

    A1132 Cut Integer

---
+++ PAT/Advanced/A1132 Cut Integer.cpp
@@ -0,0 +1,16 @@
+// https://pintia.cn/problem-sets/994805342720868352/problems/994805347145859072
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        string s;
+        cin >> s;
+        int number = stoi(s), a = stoi(s.substr(0, s.length() / 2)),
+            b = stoi(s.substr(s.length() / 2));
+        if (a * b != 0 && number % (a * b) == 0) printf("Yes\n");
+        else printf("No\n");
+    }
+    return 0;
+}
