66525d641af225762772e457af0836c26cb70b74
Zhichao Jin <jinzcdev@icloud.com>
2019-12-31 19:27:48 +0800

    A1154 Vertex Coloring

--- PAT/Advanced/Graph/A1154 Vertex Coloring.cpp
+++ PAT/Advanced/Graph/A1154 Vertex Coloring.cpp
@@ -2,47 +2,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 10010;
-vector<int> g[N];
-bool vis[N] = {false}, flag;
-int color[N];
-void dfs(int u) {
-    vis[u] = true;
-    for (int i = 0; i < g[u].size(); i++) {
-        if (!vis[g[u][i]]) {
-            if (color[u] == color[g[u][i]]) flag = false;
-            dfs(g[u][i]);
-        }
-    }
-}
 int main() {
     int n, m, u, v, k, c;
     scanf("%d%d", &n, &m);
+    vector<pair<int, int>> e;
     while (m--) {
         scanf("%d%d", &u, &v);
-        g[u].push_back(v);
-        g[v].push_back(u);
+        e.push_back({u, v});
     }
     scanf("%d", &k);
+    int color[N];
     while (k--) {
-        // bool ht[99999] = {false};
-        map<int, int> mp;
-        fill(vis, vis + n, false);
-        int cnt = 0;
-        flag = true;
+        unordered_set<int> s;
+        bool flag = true;
         for (int i = 0; i < n; i++) {
             scanf("%d", &color[i]);
-            // if (!ht[color[i]]) {
-            //     cnt++;
-            //     ht[color[i]] = true;
-            // }
-            mp[color[i]] = 1;
+            s.insert(color[i]);
         }
-        dfs(u);
-        cnt = mp.size();
-        if (flag)
-            printf("%d-coloring\n", cnt);
-        else
-            printf("No\n");
+        for (int i = 0; i < e.size(); i++) {
+            if (color[e[i].first] == color[e[i].second]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) printf("%d-coloring\n", s.size());
+        else printf("No\n");
     }
     return 0;
 }
