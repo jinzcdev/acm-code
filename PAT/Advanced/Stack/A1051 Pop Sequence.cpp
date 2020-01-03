f281f6e97d915edb18e5139783e9157e3bc6486f
Zhichao Jin <jinzcdev@icloud.com>
2020-01-03 19:49:43 +0800

    A1051 Pop Sequence

--- PAT/Advanced/Stack/A1051 Pop Sequence.cpp
+++ PAT/Advanced/Stack/A1051 Pop Sequence.cpp
@@ -1,4 +1,34 @@
 // https://pintia.cn/problem-sets/994805342720868352/problems/994805427332562944
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int m, n, k;
+    scanf("%d%d%d", &m, &n, &k);
+    while (k--) {
+        stack<int> s;
+        vector<int> v(n);
+        bool ispush[1010] = {false}, flag = true;
+        for (int i = 0; i < n; i++) scanf("%d", &v[i]);
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; s.size() < m && j <= v[i]; j++) {
+                if (!ispush[j]) {
+                    s.push(j);
+                    ispush[j] = true;
+                }
+            }
+            if (s.top() != v[i]) {
+                printf("NO\n");
+                flag = false;
+                break;
+            }
+            s.pop();
+        }
+        if (flag) printf("YES\n");
+    }
+    return 0;
+}
+
+/*
 #include <cstdio>
 #include <stack>
 #include <vector>
@@ -33,3 +63,4 @@ int main() {
     }
     return 0;
 }
+*/
