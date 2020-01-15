6eda7e0c71e53b85b5e69dadc4bc1c9c5498b353
Zhichao Jin <jinzcdev@icloud.com>
2020-01-15 18:49:27 +0800

    B1015 德才论

--- PAT/Basic/B1015 德才论.cpp
+++ PAT/Basic/B1015 德才论.cpp
@@ -1,41 +1,31 @@
 // https://pintia.cn/problem-sets/994805260223102976/problems/994805307551629312
 // https://pintia.cn/problem-sets/994805342720868352/problems/994805410555346944
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-struct Man{
-    char id[10];
-    int virtue,talent,sum;
-    int type;
-}man[100010];
-bool cmp(Man a,Man b){
-    if(a.type != b.type) return a.type < b.type;
-    else if(a.sum != b.sum) return a.sum > b.sum;
-    else if(a.virtue != b.virtue) return a.virtue > b.virtue;
-    else return strcmp(a.id,b.id) < 0;
+struct node {
+    int id, virtue, talent, sum, type;
+};
+bool cmp(node a, node b) {
+    if (a.type != b.type) return a.type < b.type;
+    else if (a.sum != b.sum) return a.sum > b.sum;
+    else if (a.virtue != b.virtue) return a.virtue > b.virtue;
+    else return a.id < b.id;
 }
-int main(){
-    int n,L,H;
-    scanf("%d%d%d",&n,&L,&H);
-    int m = n;
-    for (int i = 0; i < n; i++){
-        scanf("%s %d %d",man[i].id,&man[i].virtue,&man[i].talent);
-        man[i].sum = man[i].virtue + man[i].talent;
-        if(man[i].virtue < L || man[i].talent < L){
-            man[i].type = 5;
-            m--;
-            continue;
-        }
-        if(man[i].virtue >= H && man[i].talent >= H) man[i].type = 1;
-        else if(man[i].virtue >= H && man[i].talent < H) man[i].type = 2;
-        else if(man[i].virtue >= man[i].talent) man[i].type = 3;
-        else man[i].type = 4;
+int main() {
+    int n, l, h, id, virtue, talent, type = 0;
+    cin >> n >> l >> h;
+    vector<node> v;
+    for (int i = 0; i < n; i++) {
+        scanf("%d%d%d", &id, &virtue, &talent);
+        if (virtue < l || talent < l) continue;
+        if (virtue >= h && talent >= h) type = 1;
+        else if (virtue >= h && talent < h) type = 2;
+        else if (virtue >= talent) type = 3;
+        else type = 4;
+        v.push_back({id, virtue, talent, virtue + talent, type});
     }
-    sort(man,man + n,cmp);
-    printf("%d\n",m);
-    for (int i = 0; i < m; i++){
-        printf("%s %d %d\n",man[i].id,man[i].virtue,man[i].talent);
-    }
+    cout << v.size() << endl;
+    sort(v.begin(), v.end(), cmp);
+    for (int i = 0; i < v.size(); i++) printf("%d %d %d\n", v[i].id, v[i].virtue, v[i].talent);
     return 0;
 }
