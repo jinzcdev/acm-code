070521987cea55e055d97398392852960d21f284
Zhichao Jin <jinzcdev@icloud.com>
2020-01-18 12:31:37 +0800

    Shortest Path | Level-order Traversal | Isomorphic

---
+++ PTA/Data Structures and Algorithms (English)/函数题/6-7 Isomorphic.c
@@ -0,0 +1,35 @@
+// https://pintia.cn/problem-sets/16/problems/702
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char ElementType;
+
+typedef struct TreeNode *Tree;
+struct TreeNode {
+    ElementType Element;
+    Tree Left;
+    Tree Right;
+};
+
+Tree BuildTree(); /* details omitted */
+
+int Isomorphic(Tree T1, Tree T2);
+
+int main() {
+    Tree T1, T2;
+    T1 = BuildTree();
+    T2 = BuildTree();
+    printf("%d\n", Isomorphic(T1, T2));
+    return 0;
+}
+
+/* Your function will be put here */
+int Isomorphic(Tree T1, Tree T2) {
+    if (T1 == NULL && T2 == NULL) return 1;
+    if ((T1 == NULL && T2 != NULL) || (T2 == NULL && T1 != NULL)) return 0;
+    if (T1->Element != T2->Element) return 0;
+    int left = 0, right = 0;
+    if (T1->Left == NULL && T2->Left == NULL) return Isomorphic(T1->Right, T1->Right);
+    else if (T1->Right == NULL && T2->Right == NULL) return Isomorphic(T1->Left, T2->Left);
+    else return Isomorphic(T1->Left, T2->Right) && Isomorphic(T1->Right, T2->Left);
+}
