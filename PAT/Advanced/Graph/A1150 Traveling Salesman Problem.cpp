00d3fd70b41c4adce8d8f563763d034c3ef4ad6f
Zhichao Jin <jinzcdev@icloud.com>
2020-01-24 09:57:49 +0800

    A1150 Traveling Salesman Problem

--- PAT/Advanced/Graph/A1150 Traveling Salesman Problem.cpp
+++ PAT/Advanced/Graph/A1150 Traveling Salesman Problem.cpp
@@ -1,6 +1,58 @@
 // https://pintia.cn/problem-sets/994805342720868352/problems/1038430013544464384
-#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 0x3fffffff;
+int n, m, k;
+int e[210][210];
+int main() {
+    fill(e[0], e[0] + 210 * 210, INF);
+    scanf("%d%d", &n, &m);
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d%d", &u, &v);
+        scanf("%d", &e[u][v]);
+        e[v][u] = e[u][v];
+    }
+    scanf("%d", &k);
+    int pnum, minDist = 0x3fffffff;
+    for (int i = 1; i <= k; i++) {
+        printf("Path %d: ", i);
+        scanf("%d", &m);
+        vector<int> path(m);
+        set<int> s;     // 用集合记录不重复的顶点数
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &path[j]);
+            s.insert(path[j]);
+        }
+        int sumdist = 0;
+        bool flag = true;
+        for (int j = 0; j < m - 1; j++) {
+            if (e[path[j]][path[j + 1]] == INF) {
+                printf("NA (Not a TS cycle)\n");
+                flag = false;
+                break;
+            }
+            sumdist += e[path[j]][path[j + 1]];
+        }
+        if (!flag) continue;
+        if (path[0] != path[m - 1] || s.size() < n) {
+            printf("%d (Not a TS cycle)\n", sumdist);
+        } else {
+            if (sumdist < minDist) {
+                minDist = sumdist;
+                pnum = i;
+            }
+            if (m - 1 == n && s.size() == n) printf("%d (TS simple cycle)\n", sumdist);
+            else printf("%d (TS cycle)\n", sumdist);
+        }
+    }
+    printf("Shortest Dist(%d) = %d\n", pnum, minDist);
+    return 0;
+}
+
+/*
 #include <algorithm>
+#include <cstdio>
 #include <set>
 using namespace std;
 const int maxn = 210;
@@ -63,3 +115,4 @@ int main() {
     printf("Shortest Dist(%d) = %d\n", ansPath, ansDist);
     return 0;
 }
+*/
