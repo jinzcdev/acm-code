ea8d74a3af338fad0025abe77bdd4bb831e9c2f1
Zhichao Jin <jinzcdev@icloud.com>
2020-01-24 09:57:10 +0800

    A1146 Topological Order

--- PAT/Advanced/Graph/A1146 Topological Order.cpp
+++ PAT/Advanced/Graph/A1146 Topological Order.cpp
@@ -1,29 +1,31 @@
 // https://pintia.cn/problem-sets/994805342720868352/problems/994805343043829760
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<int> > g;
 int main() {
-    int n, m, u, v, k;
+    int n, m, u, v;
     scanf("%d%d", &n, &m);
-    g.resize(n + 1);
-    vector<int> tin(n + 1, 0), ans;
-    while (m--) {
+    vector<vector<int> > e(n + 1);
+    vector<int> in(n + 1, 0);
+    for (int i = 0; i < m; i++) {
         scanf("%d%d", &u, &v);
-        g[u].push_back(v);
-        tin[v]++;
+        e[u].push_back(v);
+        in[v]++;
     }
+    int k;
     scanf("%d", &k);
+    vector<int> ans, top(n);
     for (int i = 0; i < k; i++) {
-        vector<int> in(tin);
         bool flag = true;
+        vector<int> tin(in);
+        for (int j = 0; j < n; j++) scanf("%d", &top[j]);
         for (int j = 0; j < n; j++) {
-            scanf("%d", &u);
-            if (in[u] != 0 && flag) {
-                ans.push_back(i);
+            if (tin[top[j]] != 0) {
                 flag = false;
+                break;
             }
-            for (auto it : g[u]) in[it]--;
+            for (auto it : e[top[j]]) tin[it]--;
         }
+        if (!flag) ans.push_back(i);
     }
     for (int i = 0; i < ans.size(); i++) {
         if (i != 0) printf(" ");
