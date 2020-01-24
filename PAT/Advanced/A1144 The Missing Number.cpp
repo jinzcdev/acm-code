f3e4112593c677e7d271a41eb4f1c7058a601bf4
Zhichao Jin <jinzcdev@icloud.com>
2020-01-24 09:56:55 +0800

    A1144 The Missing Number

--- PAT/Advanced/A1144 The Missing Number.cpp
+++ PAT/Advanced/A1144 The Missing Number.cpp
@@ -1,19 +1,16 @@
 // https://pintia.cn/problem-sets/994805342720868352/problems/994805343463260160
-#include <cstdio>
-const int maxn = 100010;
-int main(){
-	int n;
-	bool ans[maxn] = {false};
-	scanf("%d", &n);
-	int temp;
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &temp);
-		if (temp > 0 && temp <= 100000){
-			ans[temp] = true;
-		}
-	}
-	int i = 0;
-	while (ans[++i]);
-	printf("%d", i);
-	return 0;
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, temp;
+    map<int, int> hash;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &temp);
+        if (temp > 0) hash[temp] = 1;
+    }
+    int number = 1;
+    while (hash[number] == 1) number++;
+    cout << number;
+    return 0;
 }
