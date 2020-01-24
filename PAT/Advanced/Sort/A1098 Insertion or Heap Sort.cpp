aa4a8b8472765494ee79e10add88c554f4f6c91e
Zhichao Jin <jinzcdev@icloud.com>
2020-01-24 09:59:54 +0800

    A1098 Insertion or Heap Sort

--- PAT/Advanced/Sort/A1098 Insertion or Heap Sort.cpp
+++ PAT/Advanced/Sort/A1098 Insertion or Heap Sort.cpp
@@ -1,5 +1,102 @@
 // https://pintia.cn/problem-sets/994805342720868352/problems/994805368847187968
-#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+vector<int> origin, v;
+void downAdjust(int low, int high) {
+    int i = low, j = i * 2;
+    while (j <= high) {
+        if (j + 1 <= high && origin[j + 1] > origin[j]) j++;
+        if (origin[i] < origin[j]) {
+            swap(origin[i], origin[j]);
+            i = j;
+            j = i * 2;
+        } else {
+            break;
+        }
+    }
+}
+void createHeap() {
+    for (int i = n / 2; i >= 1; i--) {
+        downAdjust(i, n);
+    }
+}
+int main() {
+    cin >> n;
+    origin.resize(n + 1);
+    v.resize(n + 1);
+    for (int i = 1; i <= n; i++) scanf("%d", &origin[i]);
+    for (int i = 1; i <= n; i++) scanf("%d", &v[i]);
+    int i = 2, j;
+    while (i <= n && v[i - 1] <= v[i]) j = ++i;
+    while (i <= n && v[i] == origin[i]) i++;
+    if (i == n + 1) {
+        printf("Insertion Sort\n");
+        sort(origin.begin() + 1, origin.begin() + j + 1);
+    } else {
+        printf("Heap Sort\n");
+        createHeap();
+        bool flag = true;
+        for (int k = n; k > 1; k--) {
+            swap(origin[k], origin[1]);
+            downAdjust(1, k - 1);
+            if (flag == true) break;
+            flag = true;
+            for (int l = 1; l <= n; l++) {
+                if (origin[l] != v[l]) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+    }
+    for (int l = 1; l <= n; l++) {
+        if (l != 1) printf(" ");
+        printf("%d", origin[l]);
+    }
+    return 0;
+}
+/*
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+vector<int> origin, v;
+void downAdjust(int low, int high) {
+    int i = low, j = i * 2;
+    while (j <= high) {
+        if (j + 1 <= high && v[j + 1] > v[j]) j++;
+        if (v[i] >= v[j]) break;
+        swap(v[i], v[j]);
+        i = j;
+        j = i * 2;
+    }
+}
+int main() {
+    cin >> n;
+    origin.resize(n + 1);
+    v.resize(n + 1);
+    for (int i = 1; i <= n; i++) scanf("%d", &origin[i]);
+    for (int i = 1; i <= n; i++) scanf("%d", &v[i]);
+    int i = 2, j;
+    while (i <= n && v[i - 1] <= v[i]) j = ++i;
+    while (i <= n && v[i] == origin[i]) i++;
+    if (i == n + 1) {
+        printf("Insertion Sort\n");
+        sort(v.begin() + 1, v.begin() + j + 1);
+    } else {
+        printf("Heap Sort\n");
+        int pos = n;
+        while (pos > 2 && v[pos] >= v[1]) pos--;
+        swap(v[pos], v[1]);
+        downAdjust(1, pos - 1);
+    }
+    for (int l = 1; l <= n; l++) {
+        if (l != 1) printf(" ");
+        printf("%d", v[l]);
+    }
+    return 0;
+}*/
+/*#include <cstdio>
 #include <algorithm>
 using namespace std;
 const int maxn = 110;
@@ -90,3 +187,4 @@ int main() {
     }
     return 0;
 }
+*/
