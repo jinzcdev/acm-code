5a90f57a5c3174ce8c00c77a50cb1d4d0d8d74ac
Zhichao Jin <jinzcdev@icloud.com>
2020-01-24 09:58:03 +0800

    A1154 Vertex Coloring

--- PAT/Advanced/Graph/A1154 Vertex Coloring.cpp
+++ PAT/Advanced/Graph/A1154 Vertex Coloring.cpp
@@ -1,6 +1,38 @@
 // https://pintia.cn/problem-sets/994805342720868352/problems/1071785301894295552
 #include <bits/stdc++.h>
 using namespace std;
+int main() {
+    int n, m, k, u, v;
+    scanf("%d%d", &n, &m);
+    vector<pair<int, int> > e(m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d%d", &u, &v);
+        e[i] = pair<int, int>(u, v);
+    }
+    scanf("%d", &k);
+    while (k--) {
+        vector<int> color(n);
+        unordered_set<int> s;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &color[i]);
+            s.insert(color[i]);
+        }
+        bool flag = true;
+        for (int i = 0; i < m; i++) {
+            if (color[e[i].first] == color[e[i].second]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) printf("%d-coloring\n", s.size());
+        else printf("No\n");
+    }
+    return 0;
+}
+/*
+#include <bits/stdc++.h>
+using namespace std;
 const int N = 10010;
 int main() {
     int n, m, u, v, k, c;
@@ -30,3 +62,4 @@ int main() {
     }
     return 0;
 }
+*/
