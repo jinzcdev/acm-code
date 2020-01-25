327e178737b7d600fd36bec7e81999674b5fd255
Zhichao Jin <jinzcdev@icloud.com>
2020-01-25 08:21:20 +0800

    A1133 Splitting A Linked List

--- PAT/Advanced/LinkedList/A1133 Splitting A Linked List.cpp
+++ PAT/Advanced/LinkedList/A1133 Splitting A Linked List.cpp
@@ -1,31 +1,27 @@
 // https://pintia.cn/problem-sets/994805342720868352/problems/994805346776760320
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-const int maxn = 100010;
 struct node {
     int adr, data, next;
-} Node[maxn];
+} L[100000];
 int main() {
     int head, n, k, adr;
     scanf("%d%d%d", &head, &n, &k);
     for (int i = 0; i < n; i++) {
         scanf("%d", &adr);
-        scanf("%d%d", &Node[adr].data, &Node[adr].next);
-        Node[adr].adr = adr;
+        scanf("%d%d", &L[adr].data, &L[adr].next);
+        L[adr].adr = adr;
     }
-    vector<node> l, l1, l2, l3;
-    for (int p = head; p != -1; p = Node[p].next) l.push_back(Node[p]);
-    for (int i = 0; i < l.size(); i++) {
-        if (l[i].data < 0) l1.push_back(l[i]);
-        else if (l[i].data <= k) l2.push_back(l[i]);
-        else l3.push_back(l[i]);
+    vector<node> l, l1, l2;
+    for (int p = head; p != -1; p = L[p].next) {
+        if (L[p].data < 0) l.push_back(L[p]);
+        else if (L[p].data <= k) l1.push_back(L[p]);
+        else l2.push_back(L[p]);
     }
-    l.clear();
-    for (int i = 0; i < l1.size(); i++) l.push_back(l1[i]);
-    for (int i = 0; i < l2.size(); i++) l.push_back(l2[i]);
-    for (int i = 0; i < l3.size(); i++) l.push_back(l3[i]);
-    for (int i = 0; i < l.size() - 1; i++) printf("%05d %d %05d\n", l[i].adr, l[i].data, l[i + 1].adr);
+    for (auto it : l1) l.push_back(it);
+    for (auto it : l2) l.push_back(it);
+    for (int i = 0; i < l.size() - 1; i++)
+        printf("%05d %d %05d\n", l[i].adr, l[i].data, l[i + 1].adr);
     printf("%05d %d -1\n", l[l.size() - 1].adr, l[l.size() - 1].data);
     return 0;
 }
