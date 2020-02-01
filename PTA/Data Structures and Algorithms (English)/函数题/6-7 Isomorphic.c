fe48a01062577180d9d879528aebf72fb9568bcc
Zhichao Jin <jinzcdev@icloud.com>
2020-02-01 10:44:07 +0800

    数据结构与算法题目集: 7-3 树的同构

--- PTA/Data Structures and Algorithms (English)/函数题/6-7 Isomorphic.c
+++ PTA/Data Structures and Algorithms (English)/函数题/6-7 Isomorphic.c
@@ -26,10 +26,8 @@ int main() {
 /* Your function will be put here */
 int Isomorphic(Tree T1, Tree T2) {
     if (T1 == NULL && T2 == NULL) return 1;
-    if ((T1 == NULL && T2 != NULL) || (T2 == NULL && T1 != NULL)) return 0;
+    if (T1 == NULL || T2 == NULL) return 0;
     if (T1->Element != T2->Element) return 0;
-    int left = 0, right = 0;
-    if (T1->Left == NULL && T2->Left == NULL) return Isomorphic(T1->Right, T1->Right);
-    else if (T1->Right == NULL && T2->Right == NULL) return Isomorphic(T1->Left, T2->Left);
-    else return Isomorphic(T1->Left, T2->Right) && Isomorphic(T1->Right, T2->Left);
+    return (Isomorphic(T1->Left, T2->Left) && Isomorphic(T1->Right, T2->Right)) ||
+           Isomorphic(T1->Left, T2->Right) && Isomorphic(T1->Right, T2->Left);
 }
