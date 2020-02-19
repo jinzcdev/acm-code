7cea35ba333ebabb3308a5a2151880deede10ddf
Zhichao Jin <jinzcdev@icloud.com>
2020-02-19 21:06:26 +0800

    A1028 List Sorting

--- AlgorithmNote/算法初步之排序P87/A1028 List Sorting.cpp
+++ AlgorithmNote/算法初步之排序P87/A1028 List Sorting.cpp
@@ -1,39 +1,21 @@
 // https://pintia.cn/problem-sets/994805342720868352/problems/994805468327690240
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-const int maxn = 100010;
-int c;
-
-struct Student {
-    int id;
-    char name[10];
-    int score;
-} stu[maxn];
-
-bool cmp(Student a, Student b){
-    if (c == 1){
-        return a.id < b.id;
-    }else if(c == 2){
-        int s = strcmp(a.name, b.name);
-        if (s != 0) return s < 0;
-        else return a.id < b.id;
-    }else{
-        if (a.score == b.score) return a.id < b.id;
-        else return a.score < b.score;
-    }
+struct node {
+    int id, score;
+    string name;
+};
+int n, c;
+bool cmp(node &a, node &b) {
+    if (c == 1) return a.id < b.id;
+    else if (c == 2) return a.name != b.name ? a.name < b.name : a.id < b.id;
+    else return a.score != b.score ? a.score < b.score : a.id < b.id;
 }
-
-int main(){
-    int n;
-    scanf("%d%d",&n,&c);
-    for (int i = 0; i < n; i++){
-        scanf("%d %s %d", &stu[i].id, stu[i].name, &stu[i].score);
-    }
-    sort(stu, stu + n, cmp);
-    for (int i = 0; i < n; i++){
-        printf("%06d %s %d\n", stu[i].id, stu[i].name, stu[i].score);
-    }
+int main() {
+    scanf("%d%d", &n, &c);
+    vector<node> stu(n);
+    for (int i = 0; i < n; i++) cin >> stu[i].id >> stu[i].name >> stu[i].score;
+    sort(stu.begin(), stu.end(), cmp);
+    for (auto it : stu) printf("%06d %s %d\n", it.id, it.name.c_str(), it.score);
     return 0;
 }
