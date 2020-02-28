45d81faf56bec2b4cedbde68d8e9b3abbae4b6aa
Zhichao Jin <jinzcdev@icloud.com>
2020-02-28 21:10:13 +0800

    A1099 Build A Binary Search Tree

--- PAT/Advanced/Tree&BinaryTree/A1099 Build A Binary Search Tree.cpp
+++ PAT/Advanced/Tree&BinaryTree/A1099 Build A Binary Search Tree.cpp
@@ -1,68 +1,36 @@
-#include <cstdio>
-#include <algorithm>
-#include <queue>
+// https://pintia.cn/problem-sets/994805342720868352/problems/994805367987355648
+#include <bits/stdc++.h>
 using namespace std;
-const int maxn = 110;
+const int N = 110;
 struct node {
-    int data;
-    int lchild, rchild;
-} Node[maxn];
-
-int in[maxn], n, index = 0;
-
-bool cmp(int a, int b){
-    return a < b;
+    int data, left, right;
+} Node[N];
+int in[N], n, pos = 0;
+void inOrder(int i) {
+    if (i == -1) return;
+    inOrder(Node[i].left);
+    Node[i].data = in[pos++];
+    inOrder(Node[i].right);
 }
-
-void inOrder(int root){
-    if (root == -1) return;
-    inOrder(Node[root].lchild);
-    Node[root].data = in[index++];      // 遍历过程中进行赋值以构建二叉搜索树
-    inOrder(Node[root].rchild);
-}
-
-void levelTraverse(int root){
+void bfs(int root) {
     queue<int> q;
     q.push(root);
     int cnt = 0;
-    while (!q.empty()){
+    while (!q.empty()) {
         int now = q.front();
         q.pop();
         printf("%d", Node[now].data);
         if (++cnt < n) printf(" ");
-        if (Node[now].lchild != -1) q.push(Node[now].lchild);
-        if (Node[now].rchild != -1) q.push(Node[now].rchild);
+        if (Node[now].left != -1) q.push(Node[now].left);
+        if (Node[now].right != -1) q.push(Node[now].right);
     }
 }
-
-int main(){
+int main() {
     scanf("%d", &n);
-    for (int i = 0; i < n; i++){
-        scanf("%d%d", &Node[i].lchild, &Node[i].rchild);
-    }
-    for (int i = 0; i < n; i++){
-        scanf("%d", &in[i]);
-    }
-    sort(in, in + n, cmp);          // 升序该数组以获得中序序列
-    inOrder(0);                     // 从根结点开始做中序遍历
-    levelTraverse(0);               // 层序遍历该二叉树
+    for (int i = 0; i < n; i++) scanf("%d%d", &Node[i].left, &Node[i].right);
+    for (int i = 0; i < n; i++) scanf("%d", &in[i]);
+    sort(in, in + n);
+    inOrder(0);
+    bfs(0);
     return 0;
 }
-
-/*
-Sample Input:
-9
-1 6
-2 3
--1 -1
--1 4
-5 -1
--1 -1
-7 -1
--1 8
--1 -1
-73 45 11 58 82 25 67 38 42
-
-Sample Output:
-58 25 82 11 38 67 45 73 42
-*/
--- PAT/Advanced/UFS/A1107 Social Clusters.cpp
+++ PAT/Advanced/UFS/A1107 Social Clusters.cpp
@@ -4,10 +4,7 @@ using namespace std;
 const int N = 1010;
 int father[N];
 int findFather(int x) {
-    if (x == father[x]) return x;
-    int fa = findFather(father[x]);
-    father[x] = fa;
-    return fa;
+    return x == father[x] ? x : father[x] = findFather(father[x]);
 }
 void uni(int a, int b) {
     int faA = findFather(a);
@@ -16,21 +13,20 @@ void uni(int a, int b) {
 }
 int main() {
     for (int i = 0; i < N; i++) father[i] = i;
-    int n, k, h;
-    map<int, int> hobbyToId;
+    int n, k, h, hobby[N] = {0};
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
-        scanf("%d:", &k);
+        scanf("%d: ", &k);
         for (int j = 0; j < k; j++) {
             scanf("%d", &h);
-            if (hobbyToId[h] != 0) uni(i, hobbyToId[h]);
-            else hobbyToId[h] = i;
+            if (hobby[h] != 0) uni(i, findFather(hobby[h]));
+            else hobby[h] = i;
         }
     }
-    map<int, int> mcnt;
-    for (int i = 1; i <= n; i++) mcnt[findFather(i)]++;
+    unordered_map<int, int> mp;
     vector<int> ans;
-    for (auto it : mcnt) ans.push_back(it.second);
+    for (int i = 1; i <= n; i++) mp[findFather(i)]++;
+    for (auto it : mp) ans.push_back(it.second);
     printf("%d\n", ans.size());
     sort(ans.begin(), ans.end());
     for (int i = ans.size() - 1; i >= 0; i--) {
@@ -39,71 +35,3 @@ int main() {
     }
     return 0;
 }
-/*
-#include <algorithm>
-#include <cstdio>
-#include <vector>
-using namespace std;
-const int maxn = 1010;
-int father[maxn];
-int isRoot[maxn] = {0};     // 标记是否为某结点的根, 并记录根节点的个数
-int hobby[maxn] = {0};      // 标记某人的爱好, [index:爱好,value:人物编号]
-
-bool cmp(int a, int b){
-    return a > b;
-}
-
-int findFather(int x){
-    if (x == father[x]) return x;
-    else {
-        int F = findFather(father[x]);
-        father[x] = F;
-        return F;
-    }
-}
-
-void Union(int a, int b){
-    int faA = findFather(a);
-    int faB = findFather(b);
-    if (faA != faB){
-        father[faA] = faB;
-    }
-}
-
-int main(){
-    int n, k;
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++){
-        father[i] = i;                      // 初始化所有结点父结点为自己
-    }
-    for (int i = 1; i <= n; i++){
-        scanf("%d: ", &k);
-        int h;
-        for (int j = 0; j < k; j++){
-            scanf("%d", &h);
-            if (hobby[h] == 0){              // 如果该兴趣无人喜欢,
-则令people.i喜欢该活动h hobby[h] = i;
-            }
-            Union(i, findFather(hobby[h])); // 合并
-        }
-    }
-    for (int i = 1; i <= n; i++){
-        isRoot[findFather(i)]++;            // 根节点数量+1,
-表示某个集合内结点个数
-    }
-    vector<int> ans;
-    for (int i = 1; i <= n; i++){
-        if (isRoot[i] > 0){
-            ans.push_back(isRoot[i]);
-        }
-    }
-    sort(ans.begin(), ans.end(), cmp);
-    int len = ans.size();
-    printf("%d\n", len);
-    for (int i = 0; i < len; i++){
-        printf("%d", ans[i]);
-        if (i < len - 1) printf(" ");
-    }
-    return 0;
-}
-*/
