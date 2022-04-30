4a37d085b1cafb998fd62371ebb65177c2a2ebd3
Zhichao Jin <jinzcdev@icloud.com>
2022-04-30 12:49:56 +0800

    [Add] 团体程序设计天梯赛-练习集(部分)

---
+++ PTA/团体程序设计天梯赛-练习集/L1-001 Hello World.cpp
@@ -0,0 +1,8 @@
+// https://pintia.cn/problem-sets/994805046380707840/problems/994805147132084224
+public class Main{
+  
+  public static void main(String[] args){
+    System.out.println("Hello World!");
+  }
+}
+    
